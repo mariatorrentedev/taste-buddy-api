1a618b26aa3b3ed999a4ec7d4da42c730436b707
María Torrente <caritotorrente@gmail.com>
2020-12-20 12:06:50 -0500

    updating user seeds w/hash password

--- seeds/seed.users.sql
+++ seeds/seed.users.sql
@@ -1,11 +1,11 @@
 INSERT INTO
 users (email, password)
 VALUES (
-    'test1@gmail.com', 'astapostapulos'
+    'test1@gmail.com', 'Astapostapulos66*'
 ),
 (
-    'test2@gmail.com', 'cascabeletos'
+    'test2@gmail.com', 'cascabeletoS1324+'
 ),
 (
-    'test3@gmail.com', 'donRamon'
+    'test3@gmail.com', 'DonRamon1900!'
 );
