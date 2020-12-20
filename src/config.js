481b21fd3e7ee0259fa458275c28d6438eaff529
María Torrente <caritotorrente@gmail.com>
2020-12-20 13:53:15 -0500

    fixing dynamic client origin

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   DB_URL: process.env.DB_URL || "postgresql://carito@localhost/tastebuddy",
   API_BASE_URL:
     process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
-  CLIENT_ORIGIN: "http://localhost:3000",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
   //"https://taste-buddy-client.vercel.app/",
   JWT_SECRET: process.env.JWT_SECRET || "taste-buddy",
 };
