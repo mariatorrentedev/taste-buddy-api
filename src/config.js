72656e4b2ae4ea2e68d8c6bf461537ceb13a3af1
María Torrente <caritotorrente@gmail.com>
2021-01-01 12:34:14 -0500

    fixing import in server

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgres://qbkiqyylwhlvlq:5788b8946520d6158e64ab4aa66b74f3eba1ef9de27a1095030c1a3a83e36a55@ec2-34-237-166-54.compute-1.amazonaws.com:5432/d50c92tbmd1v5a",
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
+  API_BASE_URL: process.env.API_BASE_URL,
   CLIENT_ORIGIN:
     process.env.CLIENT_ORIGIN || "https://taste-buddy-client.vercel.app",
   JWT_SECRET: process.env.JWT_SECRET,
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const { PORT, DB_URL } = require("./config");
+const { PORT, DATABASE_URL } = require("./config");
 const knex = require("knex");
 
 const db = knex({
